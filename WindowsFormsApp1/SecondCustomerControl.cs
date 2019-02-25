f1fc976bb7efe40b665eae615c3b2a8cff5a2205
Mahmoud-Arafaa <45467513+Mahmoud-Arafaa@users.noreply.github.com>
2019-02-25 15:44:38 +0200

    Update SecondCustomerControl.cs

--- WindowsFormsApp1/SecondCustomerControl.cs
+++ WindowsFormsApp1/SecondCustomerControl.cs
@@ -32,7 +32,23 @@ namespace WindowsFormsApp1
                     city_name.Text = oMycustomclassname.data.ElementAt(0).city_name;
                     timezone.Text = oMycustomclassname.data.ElementAt(0).timezone;
 					
-					
+		    string url2 = string.Format("https://api.weatherbit.io/v2.0/forecast/daily?key=d254147c15ba46b48bc8e608389ca984&city=" + textBox1.Text);
+                    var Json2 = web.DownloadString(url2);
+                    ForecastWeather.root oMycustomclassname2 = Newtonsoft.Json.JsonConvert.DeserializeObject<ForecastWeather.root>(Json2);
+
+                    DATE1.Text = oMycustomclassname2.data.ElementAt(0).datetime;
+                    AVGTemp1.Text = oMycustomclassname2.data.ElementAt(0).temp + "\u00B0C";
+                    MAXTemp1.Text = oMycustomclassname2.data.ElementAt(0).max_temp + "\u00B0C";
+                    MINTemp1.Text = oMycustomclassname2.data.ElementAt(0).min_temp + "\u00B0C";
+                    DATE2.Text = oMycustomclassname2.data.ElementAt(1).datetime;
+                    AVGTemp2.Text = oMycustomclassname2.data.ElementAt(1).temp + "\u00B0C";
+                    MAXTemp2.Text = oMycustomclassname2.data.ElementAt(1).max_temp + "\u00B0C";
+                    MINTemp2.Text = oMycustomclassname2.data.ElementAt(1).min_temp + "\u00B0C";
+                    DATE3.Text = oMycustomclassname2.data.ElementAt(2).datetime;
+                    AVGTemp3.Text = oMycustomclassname2.data.ElementAt(2).temp + "\u00B0C";
+                    MAXTemp3.Text = oMycustomclassname2.data.ElementAt(2).max_temp + "\u00B0C";
+                    MINTemp3.Text = oMycustomclassname2.data.ElementAt(2).min_temp + "\u00B0C";
+		
                 }
             }
             catch
