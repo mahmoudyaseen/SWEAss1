8de86f03d8b6f08bd93c209dd71affcf52f9ac6c
mahmoudyaseen <39798017+mahmoudyaseen@users.noreply.github.com>
2019-02-25 15:34:14 +0200

    Update Day3Hours.cs

--- WindowsFormsApp1/Day3Hours.cs
+++ WindowsFormsApp1/Day3Hours.cs
@@ -8,7 +8,15 @@ namespace WindowsFormsApp1
 {
     class Day3Hours
     {
-       
+       public class data
+        {
+            public string temp { get; set; }
+            public string datetime { get; set; }
+        }
+        public class root
+        {
+            public List<data> data { get; set; }
+        }
 
     }
 }
