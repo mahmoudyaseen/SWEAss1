52aff33210f9b39a829161eed3605cc07da2bb7b
mohamed <45340434+MohamedShahed@users.noreply.github.com>
2019-02-25 15:52:25 +0200

    Update omdbAPI.cs

--- WindowsFormsApp1/omdbAPI.cs
+++ WindowsFormsApp1/omdbAPI.cs
@@ -8,6 +8,31 @@ namespace WindowsFormsApp1
 {
     class omdbAPI
     {
-        
+        public class Ratings
+        {
+            public string Source { get; set; }
+            public string Value { get; set; }
+        }
+        public class root
+        {
+            public string Title { get; set; }
+            public string Year { get; set; }
+            public string Rated { get; set; }
+            public string Released { get; set; }
+            public string Runtime { get; set; }
+            public string Genre { get; set; }
+            public string Director { get; set; }
+            public string Writer { get; set; }
+            public string Actors { get; set; }
+            public string Plot { get; set; }
+            public string Language { get; set; }
+            public string Country { get; set; }
+            public string Awards { get; set; }
+            public string Poster { get; set; }
+            public List<Ratings> Ratings { get; set; }
+            public string imdbRating { get; set; }
+            public string imdbVotes { get; set; }
+            public string Type { get; set; }
+        }
     }
 }
