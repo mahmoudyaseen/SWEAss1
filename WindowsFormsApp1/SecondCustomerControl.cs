61fb100fc913985cb1751fca96854619c30c0a48
Mahmoud-Arafaa <45467513+Mahmoud-Arafaa@users.noreply.github.com>
2019-02-25 15:42:47 +0200

    Update SecondCustomerControl.cs

--- WindowsFormsApp1/SecondCustomerControl.cs
+++ WindowsFormsApp1/SecondCustomerControl.cs
@@ -21,6 +21,25 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                using (WebClient web = new WebClient())
+                {
+                    string url = string.Format("https://api.weatherbit.io/v2.0/current?key=d254147c15ba46b48bc8e608389ca984&city=" + textBox1.Text);
+                    var Json = web.DownloadString(url);
+                    CurrentWeather.root oMycustomclassname = Newtonsoft.Json.JsonConvert.DeserializeObject<CurrentWeather.root>(Json);
+                    CurentTemp.Text = oMycustomclassname.data.ElementAt(0).temp+ "\u00B0C";
+                    city_name.Text = oMycustomclassname.data.ElementAt(0).city_name;
+                    timezone.Text = oMycustomclassname.data.ElementAt(0).timezone;
+					
+					
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Please enter valid name", "ERORR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
 
         }
     }
